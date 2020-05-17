666e9ce20fee3368ba3631f381a0787c6ac8e74d
yutaIto0309 <yokosingo@gmail.com>
2020-05-17 10:42:31 +0900

    BeautifulSoupの使い方

--- chapter3/scrape_by_bs4.py
+++ chapter3/scrape_by_bs4.py
@@ -1,17 +1,22 @@
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-#HTMLファイルを読み込んでBeautifulSoupオブジェクトを得る。
-with open('dp.html') as f:
-    soup = BeautifulSoup(f, 'html.parser')
+def main():
 
-    #select()メソッドでa要素のリストを取得して、個々のa要素に対して処理を行う
-    for a in soup.select('#listBook > li > a[itemprop="url"]'):
-        #a要素のhref属性から書籍のURLを取得する
-        url = urljoin('https://gihyo.jp/dp', a.get('href'))
+    #HTMLファイルを読み込んでBeautifulSoupオブジェクトを得る。
+    with open('dp.html') as f:
+        soup = BeautifulSoup(f, 'html.parser')
 
-        #書籍の名前はp要素のitemprop="name"から取得する
-        p = a.select('p[itemprop="name"]')[0]
-        title = p.text
+        #select()メソッドでa要素のリストを取得して、個々のa要素に対して処理を行う
+        for a in soup.select('#listBook > li > a[itemprop="url"]'):
+            #a要素のhref属性から書籍のURLを取得する
+            url = urljoin('https://gihyo.jp/dp', a.get('href'))
 
-        print(url, title)
+            #書籍の名前はp要素のitemprop="name"から取得する
+            p = a.select('p[itemprop="name"]')[0]
+            title = p.text
+
+            print(url, title)
+
+if __name__ == "__main__":
+    main()
